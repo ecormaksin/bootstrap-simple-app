0a18c4d4d62abf6b14e25e1b62e82c220021be10
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-09 19:06:22 +0900

    サンプルに合わせてリネーム

--- src/test/java/com/fork/baeldung/ApplicationTests.java
+++ src/test/java/com/fork/baeldung/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BootstrapSimpleAppApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
