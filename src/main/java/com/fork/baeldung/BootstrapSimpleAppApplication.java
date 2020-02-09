32ccd4f4a67667615c234d9dfa3ecb4b23fefc1d
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-09 19:05:06 +0900

    first commit

---
+++ src/main/java/com/fork/baeldung/BootstrapSimpleAppApplication.java
@@ -0,0 +1,13 @@
+package com.fork.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapSimpleAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapSimpleAppApplication.class, args);
+	}
+
+}
