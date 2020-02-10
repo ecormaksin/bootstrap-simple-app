2fb33a77bc8e6fca0a7d39a89917de859a14645e
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-10 21:28:56 +0900

    Exceptionパッケージの修正

--- src/main/java/com/baeldung/controller/BookController.java
+++ src/main/java/com/baeldung/controller/BookController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baeldung.exception.BookIdMismatchException;
 import com.baeldung.exception.BookNotFoundException;
 import com.baeldung.persistence.model.Book;
 import com.baeldung.persistence.repo.BookRepository;
--- src/main/java/com/baeldung/exception/BookIdMismatchException.java
+++ src/main/java/com/baeldung/exception/BookIdMismatchException.java
@@ -1,4 +1,4 @@
-package com.baeldung.controller;
+package com.baeldung.exception;
 
 public class BookIdMismatchException extends RuntimeException {
 	private static final long serialVersionUID = -4416001888665018927L;
