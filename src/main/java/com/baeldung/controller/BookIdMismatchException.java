1a060ce5002d155b26148c55a2eb1e05d141ce52
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-10 21:23:05 +0900

    コントローラーの追加

---
+++ src/main/java/com/baeldung/controller/BookIdMismatchException.java
@@ -0,0 +1,5 @@
+package com.baeldung.controller;
+
+public class BookIdMismatchException extends RuntimeException {
+	private static final long serialVersionUID = -4416001888665018927L;
+}
---
+++ src/main/java/com/baeldung/exception/BookNotFoundException.java
@@ -0,0 +1,5 @@
+package com.baeldung.exception;
+
+public class BookNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = -859333090052692562L;
+}
